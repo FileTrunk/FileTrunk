b2775f9082cd3b2e19f85dc27c5f94e5f979b24d
Fedor Zhydok <fedorzhydok@Fedors-Laptop.local>
2023-06-25 23:55:00 +0300

    fix: last fix

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -23,34 +23,6 @@ def test_user_with_jwt_gets_list_of_files_should_succeed_with_200(
     assert response.json()['message'] == 'Success'
     assert response.json()['data'][0]['filename'] == 'folder'
 
-
-# @pytest.mark.django_db
-# def test_user_with_jwt_deletes_file_should_succeed_with_200(
-#     client,
-#     folder,
-#     headers,
-#     celery_config,
-# ):
-#     response = client.delete(f'/api/v1/files/{folder.id}/', **headers)
-#     assert response.status_code == 200
-#     assert response.content == b'{"message":"Success"}'
-
-
-# @pytest.mark.django_db
-# def test_user_with_jwt_creates_folder_should_succeed_with_200(client, headers):
-#     response = client.post(
-#         '/api/v1/files/',
-#         **headers,
-#         data={
-#             'filename': 'folder_with_non_existing_name',
-#             'is_folder': True,
-#             'parent_id': "",
-#         },
-#     )
-#     assert response.status_code == 200
-#     assert response.content == b'{"message":"Success"}'
-
-
 @pytest.mark.django_db
 def test_user_with_jwt_gets_all_child_files_should_succeed_with_200(
     client, folder, headers, file
@@ -63,28 +35,6 @@ def test_user_with_jwt_gets_all_child_files_should_succeed_with_200(
     assert response.json()['message'] == 'Success'
     assert len(response.json()['data']) == 1
 
-
-# @pytest.mark.django_db
-# def test_user_with_jwt_creates_file_should_succeed_with_200(
-#     client,
-#     folder,
-#     headers,
-#     celery_config,
-# ):
-#     filedata = {
-#         'filename': 'file',
-#         'file_type': 'type',
-#         'parent_id': folder.id,
-#         'file_size': 20.2,
-#     }
-#     response = client.post(
-#         f'/api/v1/files/file-load/',
-#         **headers,
-#         data=filedata,
-#     )
-#     assert response.status_code == 200
-
-
 @pytest.mark.django_db
 def test_user_with_jwt_gets_download_link_should_succeed_with_200(
     client, file, headers
@@ -96,24 +46,6 @@ def test_user_with_jwt_gets_download_link_should_succeed_with_200(
     assert response.status_code == 200
     assert response.json()['message'] == 'Success'
     assert isinstance(response.json()['token'], str) is True
-
-
-# @pytest.mark.django_db
-# @patch(
-#     'main.views.FileSystemStorage.open',
-#     MagicMock(return_value="Mock file"),
-# )
-# def test_user_with_jwt_downloads_file_should_succeed_with_200(
-#     client, file_download_jwt
-# ):
-#     response = client.get(f'/api/v1/files/download/?token={file_download_jwt}')
-#     assert response.status_code == 200
-#     assert (
-#         response.headers['Content-Disposition']
-#         == 'attachment; filename=file.type'
-#     )
-#     assert response.headers['Content-Type'] == 'multipart/form-data'
-
 
 @pytest.mark.django_db
 def test_user_with_valid_data_creates_file_share_link_should_succeed_with_200(
@@ -166,25 +98,6 @@ def test_user_with_valid_jwt_disables_all_share_links_for_specific_file_should_s
     response = client.delete(f'/api/v1/files/share-load/{file.id}/', **headers)
     assert response.status_code == 200
     assert ShareLink.objects.count() == 0
-
-
-# @pytest.mark.django_db
-# def test_user_with_jwt_creates_already_existing_folder_with_200(
-#     client,
-#     headers,
-#     folder,
-# ):
-#     response = client.post(
-#         '/api/v1/files/',
-#         **headers,
-#         data={'filename': 'folder', 'is_folder': True, 'parent_id': ""},
-#     )
-#     assert response.status_code == 200
-#     assert (
-#         response.content
-#         == b'{"message":"Folder with such name already exists in current directory"}'
-#     )
-
 
 @pytest.mark.parametrize(
     'url',
